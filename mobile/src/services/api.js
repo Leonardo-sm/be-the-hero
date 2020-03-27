2fd5be01e0aca149731a18ae285ea3e1e4202b1f
Leonardo-sm <leonardo.smichelutti@gmail.com>
2020-03-27 17:46:39 -0300

    Aplicativo mobile feito

---
+++ mobile/src/services/api.js
@@ -0,0 +1,11 @@
+import 'intl';
+import 'intl/locale-data/jsonp/pt-BR';
+
+import axios from 'axios';
+
+
+const api = axios.create({
+    baseURL: 'http://192.168.0.19:3333'
+});
+
+export default api;
